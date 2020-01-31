2d837e13c1e5184371bb73ae21f2c1626a6d0dbc
NicolasAndrada <nicolas.andrada2@gmail.com>
2020-01-30 22:39:18 -0300

    modifique
    Cambie el orden y tuve que ponerle un par de cosas para que tome pero la bd es la misma

--- index.php
+++ index.php
@@ -3,11 +3,19 @@ require'Geocoding.php';
 require'conexion.php';
 use myPHPnotes\Geocoding;
 $geo = new Geocoding("AIzaSyA20jxyoX7p5SzBJvu-5-gcFIe_QaUH-9Q");
-//funcion que convierte una direccion en lat y long
-$coordinates=$geo->getCoordinates('$marcar');
-var_dump($coordinates);
-//selecciona todas las direcciones cargadas
+
+//indica que se traiga en español la direccion (si no lo pones se va todo a la chingada)
+$conexion->query("SET NAMES 'utf8'");
+
+//selecciona solo la 1er direccion cargada
  $marcar= "SELECT direccion FROM prueba";
- //$result= $conexion->query($marcar); 
+ $result= $conexion->query($marcar); 
+ $marcax= $result->fetch_assoc();
+ $direccion = $marcax['direccion'];
+
+//funcion que convierte una direccion en lat y long
+$coordinates=$geo->getCoordinates($direccion);
+var_dump($coordinates);
+
 
 ?>
